69a2552f1b6e9ec16eba2774aedf90083bd23124
sudo-odner <odner.main@gmail.com>
2026-04-03 16:51:18 +0300

    feat: add base navigation

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,5 +1,16 @@
 vim.g.mapleader = " "
 
--- vim.keymap.set('n', '<leader>e', ':Neotree float focus<CR>')
--- vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
+local keymap = vim.keymap
 
+
+-- Сохранить файл
+keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
+
+-- Выйти из файла
+keymap.set("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
+
+-- Навигация по окнам (Ctrl + hjkl)
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
