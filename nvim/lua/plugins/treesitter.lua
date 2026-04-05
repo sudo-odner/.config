038f8dd98af8570bad8fc1f00c4a1884af9e5b44
sudo-odner <odner.main@gmail.com>
2026-04-05 17:45:11 +0300

    refactor: backlog noice.lua and add nginx in formater

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,17 @@ return {
     lazy = false,
     config = function()
         local configs = require("nvim-treesitter")
-        configs.install({ "go", "lua", "vim", "vimdoc", "query", "python", "javascript" })
+        configs.install({
+            "bash",
+            "vim",
+            "vimdoc",
+            "lua",
+            "go",
+            "rust",
+            "python",
+            "javascript",
+            "nginx",
+        })
 
         -- Включение подсветки через автокоманду
         vim.api.nvim_create_autocmd("FileType", {
