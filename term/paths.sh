99593561668e8a43b7ed7210858fd8e64bcfad26
sudo-odner <odner.main@gmail.com>
2026-04-13 15:31:45 +0300

    feat: add path setting

---
+++ term/paths.sh
@@ -0,0 +1,6 @@
+# --- МОЙ МАСТЕР-КОНФИГ ПУТЕЙ ---
+
+# turn - bin https://github.com/cacggghp/vk-turn-proxy
+export PATH="$HOME/.custom_bin:$PATH"
+
+echo "Кастомные пути импортированы .config/term"
